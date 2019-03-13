8b14670615a9a3e94fd30b6a593a449fd650b305
sterlingrobot <sterlingrobot@gmail.com>
2019-03-13 16:56:17 -0600

    blocks, html content support

---
+++ src/Block.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function createMarkup(content) {
+	 return { __html: decodeHtml(content) };
+}
+
+// api uses htmlentities
+function decodeHtml(str) {
+    const map =
+    {
+      '&amp;': '&',
+      '&lt;': '<',
+      '&gt;': '>',
+      '&quot;': '"',
+      '&#039;': "'"
+    };
+    return str.replace(new RegExp(Object.keys(map).join('|'), 'g'), (m) => map[m]);
+}
+
+const Block  = ({ content, classArr=[] }) =>
+	<div
+		className={ classArr.reduce((str, cls) => [str, cls].join(' '), '') }
+		dangerouslySetInnerHTML={createMarkup(content)}
+	>
+	</div>
+
+export default Block;

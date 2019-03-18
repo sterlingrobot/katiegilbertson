21504cf9e81fe09f5f0d3e1fc39e44c57d2ad2f6
sterlingrobot <sterlingrobot@gmail.com>
2019-03-18 00:33:47 -0600

    contact pg

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,20 +1,55 @@
-import React from 'react';
-import Icon from './Icon.jsx';
+import React, { Component } from 'react';
 
+import Icon from './Icon.jsx';
 import Button from './Button';
+
+import contactImg from './images/profile-contact.jpg';
 
 import './Contact.scss';
 
 const ContactItem = ({ name, icon, url, content }) =>
 	<li data-type={name} >
-		<Button type="icon" size="sm" text={content} url={url} icons={[Icon[icon]]} />
+		<Button type="icon" size="lg" text={content} url={url} icons={[Icon[icon]]} />
 	</li>
 
-const Contact = ({ contacts }) =>
+const ContactList = ({ contacts }) =>
 	<ul className="contacts">
 		{ Object.keys(contacts).map((contact, i) =>
 			<ContactItem key={i} name={contact} { ...contacts[contact] } />
 		)}
 	</ul>
+
+class Contact extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			contacts: {}
+		}
+	}
+
+	componentDidMount() {
+		fetch(/development/.test(process.env.NODE_ENV) ? '//api.katie.local:8005/contacts.json' : '//api.katiegilbertson.com/contacts.json')
+			.then(response => response.json())
+			.then(data => this.setState({
+				contacts: data
+			}));
+	}
+
+	render() {
+		return (
+			<section className="contact">
+				<div className="contact-image" style={{ backgroundImage: `url(${contactImg})` }} />
+				<div className="contact-content">
+					<p>Need help finding your story?
+					<br/>Or do you already know it?
+					<br/>I can help from story development through editing.</p>
+					<h3>Contact Me</h3>
+				</div>
+				<ContactList contacts={this.state.contacts} />
+			</section>
+		)
+	}
+}
 
 export default Contact;

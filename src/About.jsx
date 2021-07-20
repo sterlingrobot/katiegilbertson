e2b3bee19ff2b7bb678043845cfcf8e591691618
Tor Gilbertson <sterlingrobot@gmail.com>
2021-07-19 19:48:59 -0600

    update bio

--- src/About.jsx
+++ src/About.jsx
@@ -7,8 +7,8 @@ const About = () =>
 	<section className="about">
 		<div className="about-image" style={{ backgroundImage: `url(${aboutImg})` }} />
 		<div className="about-content">
-			<p>An award winning documentary filmmaker and editor based in beautiful Bozeman, Montana. Her work has screened on PBS, TERRA, Current TV, National Parks, and in numerous film festivals worldwide. Additionally, Stories of Trust is being used across the country to promote public engagement in climate change, and is a tool in ongoing litigation for a Climate Recovery Plan that is based on science and human rights, not political agenda.</p>
-			<p>Katie holds an MFA in Science and Natural History Filmmaking, a BFA in Theatre, and a minor in biology. The fusion of these artistic mediums creates a complexity in her storytelling offering great attention to audience, emotional engagement, pacing, revelation, and story arc.</p>
+			<p>Katie is an Emmy Award winning documentary and television editor based in Bozeman, MT. Her work has screened on Smithsonian Channel, Animal Planet, PBS, Independent Lens, in National Parks, and in numerous film festivals worldwide.</p>
+			<p>She holds an MFA in Science and Natural History Filmmaking and a BFA in theatre/dance. The fusion of these artistic mediums weaves character, movement, emotional engagement, pacing, and story arc into compelling journeys.</p>
 		</div>
 	</section>
 

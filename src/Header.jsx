23956e4f2d5a240601b641c00710bfbea4d0da43
Tor Gilbertson <sterlingrobot@gmail.com>
2021-07-19 20:28:58 -0600

    fix company name

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import './Header.scss';
 
 const Header = ({ children }) =>
 	<header className="app-header">
-		<h1 className="company-name">Story Architech</h1>
+		<h1 className="company-name">story <span>ARC</span>itech</h1>
 		<h2>Katie Lose Gilbertson</h2>
 		<h5>
 			<span>Filmmaker</span>

8cd6c887bde470b084e8c76b1c00239818ace5ae
Tor Gilbertson <sterlingrobot@gmail.com>
2021-07-19 22:39:29 -0600

    check empty value for is_gated

--- api/projects.php
+++ api/projects.php
@@ -16,7 +16,7 @@ $stmt = $db->prepare('SELECT id,
 	role,
 	images_folder,
 	video_link,
-	video_pswd IS NOT NULL AS is_gated,
+	LENGTH(video_pswd > 0) AS is_gated,
 	social_links,
 	sort
 	FROM projects

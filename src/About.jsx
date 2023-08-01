2260c890b521d360df8d12716e5ed0026958018d
Tor Gilbertson <sterlingrobot@gmail.com>
2023-08-01 18:04:34 -0400

    update about info

--- src/About.jsx
+++ src/About.jsx
@@ -3,13 +3,25 @@ import React from 'react';
 import './About.scss';
 import aboutImg from './images/profile-about.jpg';
 
-const About = () =>
-	<section className="about">
-		<div className="about-image" style={{ backgroundImage: `url(${aboutImg})` }} />
-		<div className="about-content">
-			<p>Katie is an Emmy Award winning documentary and television editor based in Bozeman, MT. Her work has screened on Smithsonian Channel, Animal Planet, PBS, Independent Lens, in National Parks, and in numerous film festivals worldwide.</p>
-			<p>She holds an MFA in Science and Natural History Filmmaking and a BFA in theatre/dance. The fusion of these artistic mediums weaves character, movement, emotional engagement, pacing, and story arc into compelling journeys.</p>
-		</div>
-	</section>
+const About = () => (
+    <section className="about">
+        <div className="about-image" style={{ backgroundImage: `url(${aboutImg})` }} />
+        <div className="about-content">
+            <p>
+                Katie Lose Gilbertson is an Emmy Award winning film and television editor. Her work has screened on
+                Smithsonian Channel, Animal Planet, Disney, PBS, Independent Lens, in National Parks, and in numerous
+                film festivals worldwide.
+            </p>
+            <p>
+                Katie has 16 years experience crafting natural history films and documentaries. After receiving a
+                Bachelor of Fine Arts Degree in Acting and Directing (Theatre/Dance) with a minor in Biology, she went
+                on to a Master of Fine Arts in Science and Natural History Filmmaking where she discovered and honed her
+                talents as a storyteller. The fusion of these artistic mediums weaves character, movement, emotional
+                engagement, pacing, and story arc into compelling journeys. Katie is an Editor, a Director, and a
+                Producer. She is a storyteller.
+            </p>
+        </div>
+    </section>
+);
 
 export default About;
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 
 import './Header.scss';
 
-const Header = ({ children }) =>
-	<header className="app-header">
-		<h1 className="company-name">story <span>ARC</span>itech</h1>
-		<h2>Katie Lose Gilbertson</h2>
-		<h5>
-			<span>Filmmaker</span>
-			<span>Editor</span>
-			<span>Story Consultant</span>
-		</h5>
-		{children}
-		<h6>Bozeman, Montana</h6>
-	</header>
+const Header = ({ children }) => (
+    <header className="app-header">
+        <h1 className="company-name">
+            story <span>ARC</span>itech
+        </h1>
+        <h2>Katie Lose Gilbertson</h2>
+        <h5>
+            <span>Filmmaker</span>
+            <span>Editor</span>
+            <span>Story Consultant</span>
+        </h5>
+        {children}
+        <h6>
+            <span>Bozeman, Montana</span>
+            <span>Louisville, Kentucky</span>
+        </h6>
+    </header>
+);
 
 export default Header;
